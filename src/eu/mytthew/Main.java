39ed12385ae416a6ec10815c2e9c6a56bee7fee4
Mytthew <mateuszd@haddad.com>
2021-04-11 12:45:35 +0200

    Initial commit

---
+++ src/eu/mytthew/Main.java
@@ -0,0 +1,101 @@
+package eu.mytthew;
+
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // LAB1 EX1
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Podaj imie:");
+//        String name = scanner.nextLine();
+//        System.out.println("Podaj nazwisko:");
+//        String surname = scanner.nextLine();
+//        System.out.println("Podaj kierunek:");
+//        String collegeInfo = scanner.nextLine();
+//        System.out.println("Podaj na ktorym roku studiow jestes: ");
+//        String collegeYear = scanner.nextLine();
+//        System.out.println("Podaj nazwe szkoly: ");
+//        String collegeName = scanner.nextLine();
+//        System.out.println(LocalDate.now());
+//        System.out.println("Imie = " + name);
+//        System.out.println("Nazwisko = " + surname);
+//        System.out.println("Twoja uczelnia to " + collegeName + " studiujesz na " + collegeYear + "roku. Twoj kierunek to: " + collegeInfo);
+//        int numberOfStudents = 100;
+//        System.out.println("Number of students = " + numberOfStudents);
+
+        // LAB 1 EX2
+//        final String communicatAboutName = "Podaj imie:";
+//        final String communicatAboutSurname = "Podaj nazwisko:";
+//        final String communicatAboutMajor = "Podaj kierunek:";
+//        final String communicatAboutYear = "Podaj rok studiow:";
+//        final String communicatAboutCollege = "Podaj nazwe szkoly:";
+//        System.out.println(communicatAboutName);
+//        String name = scanner.nextLine();
+//        System.out.println(communicatAboutSurname);
+//        String surname = scanner.nextLine();
+//        System.out.println(communicatAboutMajor);
+//        String collegeInfo = scanner.nextLine();
+//        System.out.println(communicatAboutYear);
+//        String collegeYear = scanner.nextLine();
+//        System.out.println(communicatAboutCollege);
+//        String collegeName = scanner.nextLine();
+//        System.out.println(LocalDate.now());
+//        System.out.println("Imie = " + name);
+//        System.out.println("Nazwisko = " + surname);
+//        System.out.println("Twoja uczelnia to " + collegeName + " studiujesz na " + collegeYear + " roku. Twoj kierunek to: " + collegeInfo);
+//        int numberOfStudents = 100;
+//        System.out.println("Number of students = " + numberOfStudents);
+
+        // LAB 1 EX3
+//        final double minimumWage = 2500;
+//        double firstWage = 2200;
+//        double secondWage = 1200;
+//        double thirdWage = 2000;
+//        System.out.println("Pierwsza kwota jest mniejsza o " + (minimumWage - firstWage) + " od najnizszej krajowej.");
+//        System.out.println("Druga kwota jest mniejsza o " + (minimumWage - secondWage) + " od najnizszej krajowej.");
+//        System.out.println("Trzecia kwota jest mniejsza o " + (minimumWage - thirdWage) + " od najnizszej krajowej.");
+
+        // LAB 1 EX4
+//        final double kelwin = 274.1500;
+//        final double fahrenheit = 33.80000;
+//        System.out.println("Podaj stopnie Celsjusza: ");
+//        double celsius = scanner.nextInt();
+//        System.out.println(celsius + " stopni Celsjusza to " + (celsius + 273.15) + " stopni Kelwina");
+//        System.out.println(celsius + " stopni Celsjusza to " + ((9.0/5.0) * celsius + 32) + " stopni Fahrenheita");
+
+        // LAB 1 EX5
+//        final double euro = 4.54;
+//        final double funt = 5.23;
+//        final double dolar = 3.82;
+//        double value1 = 1285;
+//        double value2 = 15;
+//        double value3 = 100;
+//        DecimalFormat df = new DecimalFormat("####.##");
+//        System.out.println(value1 + " PLN = " + df.format(euro * value1) + " Euro.");
+//        System.out.println(value1 + " PLN = " + df.format(funt * value1) + " Funtow.");
+//        System.out.println(value1 + " PLN = " + df.format(dolar * value1) + " Dolarow.");
+//        System.out.println(value2 + " PLN = " + df.format(euro * value2) + " Euro.");
+//        System.out.println(value2 + " PLN = " + df.format(funt * value2) + " Funtow.");
+//        System.out.println(value2 + " PLN = " + df.format(dolar * value2) + " Dolarow.");
+//        System.out.println(value3 + " PLN = " + df.format(euro * value3) + " Euro.");
+//        System.out.println(value3 + " PLN = " + df.format(funt * value3) + " Funtow.");
+//        System.out.println(value3 + " PLN = " + df.format(dolar * value3) + " Dolarow.");
+
+        // LAB 1 EX6
+//        DecimalFormat df = new DecimalFormat("##.##");
+//        final double pi = 3.14;
+//        double firstCircle = 4;
+//        double secondCircle = 4.12;
+//        double thirdCircle = 12.57;
+//        System.out.println("Obwod kola o promieniu " + firstCircle + " wynosi " + df.format(2 * firstCircle * pi));
+//        System.out.println("Pole powierzchni kola o promieniu " + firstCircle + " wynosi " + df.format(pi * Math.pow(firstCircle, 2)));
+//        System.out.println("Obwod kola o promieniu " + secondCircle + " wynosi " + df.format(2 * secondCircle * pi));
+//        System.out.println("Pole powierzchni kola o promieniu " + secondCircle + " wynosi " + df.format(pi * Math.pow(secondCircle, 2)));
+//        System.out.println("Obwod kola o promieniu " + thirdCircle + " wynosi " + df.format(2 * thirdCircle * pi));
+//        System.out.println("Pole powierzchni kola o promieniu " + thirdCircle + " wynosi " + df.format(pi * Math.pow(thirdCircle, 2)));
+    }
+}
